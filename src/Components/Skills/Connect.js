4489e5b6e2e1e7d016b6d008927e6a2331fb9f98
Forrest Bingham <bingham_5@live.com>
2020-06-08 15:48:56 -0600

    Finished Links, added styling

--- src/Components/Skills/Connect.js
+++ src/Components/Skills/Connect.js
@@ -9,13 +9,17 @@ const Connect = () => {
     return (
         <IconContext.Provider value={{ style: {color: "#4abc76", height: "60px", width: "60px" }}}> 
         <div className="connect">
-            <h1> CONNECT WITH ME</h1>
+            <h3> CONNECT WITH ME</h3>
             <div className="links">
                 <div className="link">
+                <a href="https://github.com/Forrest-Bingham" rel="noopener noreferrer" target="_blank">
                 <FaGithub/>
+                </a>
                 </div>
                 <div className="link">
+                <a href="https://www.linkedin.com/in/forrest-bingham-8b3a091a8/" rel="noopener noreferrer" target="_blank">
                 <FaLinkedin/>
+                </a>
                 </div>
                 <div className="link">
                 <a href="mailto:bingham_5@live.com?subject=Hello!">
