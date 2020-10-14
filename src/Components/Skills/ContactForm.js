b8f22e331aaa870f323895e362fca15bd94af5b9
Forrest Bingham <bingham_5@live.com>
2020-10-13 19:08:25 -0600

    Finished contact form and notification

--- src/Components/Skills/ContactForm.js
+++ src/Components/Skills/ContactForm.js
@@ -9,16 +9,20 @@ const ContactForm = () => {
 
     const handleSubmit = e => {
         e.preventDefault();
+        console.log("Thanks for sending me a message")
+        var x = document.getElementById("snackbar");
+        x.className = "show";
+        setTimeout(function(){ x.className = x.className.replace("show", ""); }, 5000);
 
         setName("")
         setEmail("")
         setMessage("")
     }
-
+    
     return (
         <div className="formSpace">
-
-            <h2>Contact Me</h2>
+            <div id="snackbar">Thanks for sending me a message! This feature is currently under maintenance. Please use the email link below.</div>
+            <h2>Let's Connect</h2>
             <div>
             <form onSubmit={handleSubmit} className="form">
                 <input
